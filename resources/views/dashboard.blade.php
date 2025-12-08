3c4c0d8b1579e57334c383d6c389828ace41385b
Dave Damondamon <davedamon94@gmail.com>
2025-12-08 12:33:44 +0800

    added chart.js script

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -496,7 +496,7 @@
 @endpush
 
 @push('scripts')
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="{{ asset('js/vendor/chart.js') }}"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         let salesChart;
